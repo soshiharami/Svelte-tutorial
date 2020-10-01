f7f6d3f3ad5a9aef7104e48a1f707c505c9ce90c
soshiharami <soshi0807sss@gmail.com>
2020-10-01 12:08:14 +0900

    add SPA

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,19 @@
+<div>
+	<ul>
+		<li><Router {routes}/></li>
+	</ul>
+</div>
+
 <script>
-	let name = "soshi";
+    import Router from 'svelte-spa-router'
+    import ToDoList from './ToDoList.svelte'
+	import home from './home.svelte'
+
+    const routes = {
+        '/': home,
+        '/ToDoList': ToDoList,
+    }
 </script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
---
+++ src/home.svelte
@@ -0,0 +1,6 @@
+<script>
+    import {link} from 'svelte-spa-router'
+</script>
+
+<h1>Homepage</h1>
+<a href="/ToDoList" use:link>ToDoList</a>
